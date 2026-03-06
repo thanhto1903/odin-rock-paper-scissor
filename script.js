94f37ff3304f7fa7ff5888e0e4b5e53557d7de97
thanhtd <thanhtd@sabitech.vn>
2026-03-06 10:14:23 +0700

    add getComputerChoice func

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  let randomInt = Math.floor(Math.random() * 3);
+  //   console.log(randomInt); For test return randomInt
+  if (randomInt === 0) {
+    console.log("rock");
+  } else if (randomInt === 1) {
+    console.log("paper");
+  } else {
+    console.log("scissors");
+  }
+}
